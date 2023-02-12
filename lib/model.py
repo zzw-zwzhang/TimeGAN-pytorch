35247767dfe538077d0ca1eab8926ce366e468f9
Biaolin Wen <robinbg@foxmail.com>
2023-02-13 03:22:49 +0800

    Update model.py

--- lib/model.py
+++ lib/model.py
@@ -27,13 +27,22 @@ import torch.nn.init as init
 
 def _weights_init(m):
     classname = m.__class__.__name__
-    if isinstance(m, nn.Linear) or isinstance(m, nn.Conv2d):
-        init.kaiming_normal_(m.weight)
+    if isinstance(m, nn.Linear):
+        init.xavier_uniform_(m.weight)
+        m.bias.data.fill_(0)
     elif classname.find('Conv') != -1:
         m.weight.data.normal_(0.0, 0.02)
-    elif classname.find('BatchNorm') != -1:
+    elif classname.find('Norm') != -1:
         m.weight.data.normal_(1.0, 0.02)
         m.bias.data.fill_(0)
+    elif classname.find("GRU") != -1:
+      for name,param in m.named_parameters():
+        if 'weight_ih' in name:
+          init.xavier_uniform_(param.data)
+        elif 'weight_hh' in name:
+          init.orthogonal_(param.data)
+        elif 'bias' in name:
+          param.data.fill_(0)
 
 
 class Encoder(nn.Module):
@@ -49,6 +58,7 @@ class Encoder(nn.Module):
     def __init__(self, opt):
         super(Encoder, self).__init__()
         self.rnn = nn.GRU(input_size=opt.z_dim, hidden_size=opt.hidden_dim, num_layers=opt.num_layer)
+       # self.norm = nn.BatchNorm1d(opt.hidden_dim)
         self.fc = nn.Linear(opt.hidden_dim, opt.hidden_dim)
         self.sigmoid = nn.Sigmoid()
         self.apply(_weights_init)
@@ -74,6 +84,8 @@ class Recovery(nn.Module):
     def __init__(self, opt):
         super(Recovery, self).__init__()
         self.rnn = nn.GRU(input_size=opt.hidden_dim, hidden_size=opt.z_dim, num_layers=opt.num_layer)
+        
+      #  self.norm = nn.BatchNorm1d(opt.z_dim)
         self.fc = nn.Linear(opt.z_dim, opt.z_dim)
         self.sigmoid = nn.Sigmoid()
         self.apply(_weights_init)
@@ -99,12 +111,14 @@ class Generator(nn.Module):
     def __init__(self, opt):
         super(Generator, self).__init__()
         self.rnn = nn.GRU(input_size=opt.z_dim, hidden_size=opt.hidden_dim, num_layers=opt.num_layer)
+     #   self.norm = nn.LayerNorm(opt.hidden_dim)
         self.fc = nn.Linear(opt.hidden_dim, opt.hidden_dim)
         self.sigmoid = nn.Sigmoid()
         self.apply(_weights_init)
 
     def forward(self, input, sigmoid=True):
         g_outputs, _ = self.rnn(input)
+      #  g_outputs = self.norm(g_outputs)
         E = self.fc(g_outputs)
         if sigmoid:
             E = self.sigmoid(E)
@@ -124,12 +138,14 @@ class Supervisor(nn.Module):
     def __init__(self, opt):
         super(Supervisor, self).__init__()
         self.rnn = nn.GRU(input_size=opt.hidden_dim, hidden_size=opt.hidden_dim, num_layers=opt.num_layer)
+      #  self.norm = nn.LayerNorm(opt.hidden_dim)
         self.fc = nn.Linear(opt.hidden_dim, opt.hidden_dim)
         self.sigmoid = nn.Sigmoid()
         self.apply(_weights_init)
 
     def forward(self, input, sigmoid=True):
         s_outputs, _ = self.rnn(input)
+      #  s_outputs = self.norm(s_outputs)
         S = self.fc(s_outputs)
         if sigmoid:
             S = self.sigmoid(S)
@@ -149,13 +165,14 @@ class Discriminator(nn.Module):
     def __init__(self, opt):
         super(Discriminator, self).__init__()
         self.rnn = nn.GRU(input_size=opt.hidden_dim, hidden_size=opt.hidden_dim, num_layers=opt.num_layer)
+      #  self.norm = nn.LayerNorm(opt.hidden_dim)
         self.fc = nn.Linear(opt.hidden_dim, opt.hidden_dim)
         self.sigmoid = nn.Sigmoid()
         self.apply(_weights_init)
 
-    def forward(self, input, sigmoid=False):
+    def forward(self, input, sigmoid=True):
         d_outputs, _ = self.rnn(input)
         Y_hat = self.fc(d_outputs)
         if sigmoid:
-            Y_hat = self.igmoid(Y_hat)
+            Y_hat = self.sigmoid(Y_hat)
         return Y_hat
